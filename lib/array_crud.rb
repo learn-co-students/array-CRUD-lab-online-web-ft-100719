00f424bfa1cb9a16eaea6fbd5e1e3b55078007dd
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-11 01:39:00 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,44 @@
-def create_an_empty_array
-  
+ 
+ def
+ create_an_empty_array = [ ]
 end
 
-def create_an_array
-  
+def
+create_an_array = [1,2,3,4]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def
+ add_element_to_end_of_array = [1,2,3,4]
+  add_element_to_end_of_array << 5
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def 
+  add_element_to_start_of_array = ["b","c","d"]
+  add_element_to_start_of_array.unshift("a")
 end
 
-def remove_element_from_end_of_array(array)
+def 
+  remove_element_from_end_of_array = [1,2,3,4,5]
+  remove_element_from_end_of_array.pop
+end 
   
+
+def 
+  remove_element_from_start_of_array = [1,2,3,4]
+  remove_element_from_start_of_array.shift
 end
 
-def remove_element_from_start_of_array(array)
-  
+def 
+  retrieve_element_from_index=["a","b","c","d"] 
+  retrieve_element_from_index[-1]
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def 
+  retrieve_first_element_from_array=[1,2,3,4]
+  retrieve_first_element_from_array.first
 end
 
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
+def 
+  retrieve_last_element_from_array=[1,2,3,4]
+  retrieve_last_element_from_array.last
 end
